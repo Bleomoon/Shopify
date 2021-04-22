930e2b808450b4a4a3ed2d5c5de642e1be1cf9ba
Rémi Orveau <78747414+Bleomoon@users.noreply.github.com>
2021-04-22 14:56:43 +0200

    first javas

--- GracietTheme/assets/application.js
+++ GracietTheme/assets/application.js
@@ -1,1 +1,16 @@
-// Put your application javascript here
+function openMenu(){
+    const navbar = document.querySelector('.navbar');
+    const navbarBtn = document.querySelector('.navbar-toggler');
+
+    /*Ici on crée un type open, qui s'il est présent alors c'est ouvert, sinon non */
+    if(navbar.classList.contains('open'))
+    {
+        navbar.classList.remove('open');
+        navbarBtn.innerHTML = '<i class="fas fa-bars"></i>';
+    }
+    else
+    {
+        navbar.classList.add('open');
+        navbarBtn.innerHTML = '<i class="fas fa-times"></i>'
+    }
+}
