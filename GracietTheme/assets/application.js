a45787c9f6164658b0866bb52162dfc34c20f967
Rémi Orveau <78747414+Bleomoon@users.noreply.github.com>
2021-04-23 14:22:31 +0200

    oui

--- GracietTheme/assets/application.js
+++ GracietTheme/assets/application.js
@@ -43,18 +43,18 @@ function openUser()
 }
 
 function openMenuNavSmallScreen(){
-    const navbar = document.querySelector('.smallScreen-user');
-    const navbarBtn = document.querySelector('.smallScreen-toggler');
+    const navbarSmallScreen = document.querySelector('.smallScreen');
+    const navbarSmallScreenBtn = document.querySelector('.smallScreen-toggler');
 
     /*Ici on crée un type open, qui s'il est présent alors c'est ouvert, sinon non */
-    if(navbar.classList.contains('open'))
+    if (navbarSmallScreen.classList.contains('open'))
     {
-        navbar.classList.remove('open');
-        navbarBtn.innerHTML = '<i class="fas fa-bars"></i>';
+        navbarSmallScreen.classList.remove('open');
+        navbarSmallScreenBtn.innerHTML = '<i class="fas fa-bars"></i>';
     }
     else
     {
-        navbar.classList.add('open');
-        navbarBtn.innerHTML = '<i class="fas fa-times"></i>'
+        navbarSmallScreen.classList.add('open');
+        navbarSmallScreenBtn.innerHTML = '<i class="fas fa-times"></i>'
     }
 }
